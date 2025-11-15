b92a1dd10e4886e02497ff00049dba4b34526f8e
Helen <kozovyakina@gmail.com>
2025-11-15 21:20:00 +0300

    base

---
+++ Lessons/src/lesson_1/Input.java
@@ -0,0 +1,33 @@
+package lesson_1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("Возраст: ");
+//        int age = sc.nextInt();
+//        System.out.print("Рост: ");
+//        float height = sc.nextFloat();
+//        sc.nextLine();
+//        System.out.print("Имя: ");
+//        String name = sc.nextLine();
+//
+//        System.out.printf("Имя: %s. Возраст: %d. Рост: %.2f", name, age, height);
+
+//        System.out.print("Возраст: ");
+//        int age = sc.nextInt();
+//        System.out.print("Рост: ");
+//        float height = sc.nextFloat();
+//        System.out.print("Имя: ");
+//        String name = sc.next();
+//
+//        System.out.printf("Имя: %s. Возраст: %d. Рост: %.2f", name, age, height);
+
+//        System.out.print("Имя: ");
+////        String name = sc.next();  // Мария
+//        String name = sc.nextLine();  // Мария Петрова
+//        System.out.println(name);
+        sc.close();
+    }
+}
---
+++ Lessons/src/lesson_1/Main.java
@@ -0,0 +1,175 @@
+package lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//        System.out.println("Привет");
+//        System.out.print("Hello");
+//        System.out.print("World");
+//        System.out.println();
+
+//        int a;
+//        a = 10;
+//        System.out.println(a);
+//
+//        var b = 5;
+//        System.out.println(b);
+
+//        final int WEEK;
+//        WEEK = 7;
+
+//        System.out.println(Boolean.TRUE);
+//        System.out.println(Boolean.TYPE);
+//        System.out.println(true);
+
+//        char ch = '*';
+//        System.out.println(ch);
+//        char ch2 = 1084;
+//        System.out.println(ch2);
+//        System.out.println(Character.SIZE);
+//        System.out.println(Character.BYTES);
+
+//        byte a = 127;
+//        System.out.println(a);
+//        System.out.println(Byte.SIZE);
+//        System.out.println(Byte.BYTES);
+//        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+
+//        float a = 8.5F;
+//        System.out.println(a);
+//
+//        double b = 8.5;
+//        System.out.println(b);
+//
+//        var c = 8.5;
+//        System.out.println(c);
+//
+//        long d = 12345678901L;
+//        System.out.println(d);
+//        System.out.println(((Object)512L).getClass().getSimpleName());
+//        int num16 = 0x12;
+//        int num8 = 022;
+//        int num2 = 0b10010;
+//        System.out.println(num16);
+//        System.out.println(num8);
+//        System.out.println(num2);
+//        System.out.println(num2 + num16);
+
+//        int x = 123_456;
+//        int y = 123_456__789;
+////        double z = 123_567.789__102;
+////        double z = 0.789__102;
+////        double z = .789__102;
+//        double z = 789__102.;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+
+//        String text = "Hello \nWorld";
+//        System.out.println(text);
+
+//        System.out.print("5 + 3 = ");
+//        System.out.println(5 + 3);
+//
+//        System.out.printf("5 + 3 = %d \n", 5 + 3);
+
+//        String name = "Ирина";
+//        int age = 18;
+//        float height = 1.7f;
+//        System.out.printf("Меня зовут %s. Мне %d лет. Мой рост %.2f %n", name, age, height);
+//        System.out.printf("%8d%n", 123);
+//        System.out.printf("%4d%5d%n", 123, 456);
+//        System.out.printf("%4d%n", 123456789);
+//        System.out.printf("%6.2f%n", 123.45);
+//        System.out.printf("%8.2f%n", 123.45);
+//        System.out.printf("%-6s%5d%n", "num =", 456);
+
+//        System.out.println("------------------------------------");
+//        System.out.printf("| %-10s   | %-8s    | $4s |%n", "CATEGORY", "NAME", "BITS");
+//        System.out.println("------------------------------------");
+//        System.out.printf("| %-10s   | %-8s    | $4s |%n", "Floating", "double", 64);
+//        System.out.printf("| %-10s   | %-8s    | $4s |%n", "Floating", "float", 32);
+//        System.out.printf("| %-10s   | %-8s    | $4s |%n", "Integer", "int", 32);
+//        System.out.println("------------------------------------");
+
+//        System.out.println(5 / 2);
+//        System.out.println(5.0 / 2);
+
+//        int num = 4321;
+//        int a, b, c, d, res;
+//        System.out.println(num);
+//        a = num / 1000;
+//        System.out.println(a);
+//        b = num / 100 % 10;
+//        System.out.println(b);
+//        c = num / 10 % 10;
+//        System.out.println(c);
+//        d = num % 10;
+//        System.out.println(d);
+//        res = d * 1000 + c * 100 + b * 10 + a;
+//        System.out.println(res);
+
+//        int a = 8;
+//        int b = 8;
+//        a++;
+//        ++b;
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        int a = 8;
+//        int b = a++;
+//
+//        System.out.println(a);  // 9
+//        System.out.println(b);  // 8  9
+
+//        int a = 1;
+//        int b = 1;
+//        int c = a++ + 3;
+//        int d = ++b + 3;
+//        System.out.println(a);  // 2
+//        System.out.println(b);  // 2
+//        System.out.println(c);  // 4
+//        System.out.println(d);  // 5
+
+//        int a = 2;
+//        int b = a++;
+//        int c = b + 3 + a;
+//        System.out.println(c); // 2 + 3 + 3 = 8
+
+//        int a = 2;
+//        int b = ++a;
+//        int c = b + 3 + a;
+//        System.out.println(c); // 3 + 3 + 3 = 9
+
+//        int x = 5;
+//        byte y = (byte) x;
+//        System.out.println(y);
+
+//        byte x = 5;
+//        int y = x;
+//        System.out.println(y);
+
+//        int x = 5;
+//        double y = x;
+//        System.out.println(y);
+
+//        char x = '*';
+//        System.out.println(x);
+//        int y = x;
+//        System.out.println(y);
+//        int x = 987654321;
+//        float y = x;
+//        System.out.println(y);
+
+//        int x = 25;
+//        byte y = (byte) x;
+//        System.out.println(y);
+
+//        long x = 25;
+//        int y = (int) x;
+//        System.out.println(y);
+
+
+    }
+}
